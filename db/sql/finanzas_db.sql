ddfff45601028be5fc77bbe72a8f267505779871
gregori bonifas <gregoribonifas@gmail.com>
2025-06-17 23:55:32 -0500

    se actualizo BD finanzas_db

--- db/sql/finanzas_db.sql
+++ db/sql/finanzas_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2025 a las 21:56:03
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Tiempo de generación: 18-06-2025 a las 06:52:56
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `gestion_fp`
+-- Base de datos: `finanzas_db`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `tipo` enum('ingreso','gasto') NOT NULL
+  `tipo` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -38,8 +38,16 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `tipo`) VALUES
-(4, 'Casa', 'gasto'),
-(5, 'Regalo', 'ingreso');
+(4, 'Servicios Basicos', 'ingreso'),
+(5, 'Regalo', 'ingreso'),
+(6, 'servios', 'gasto'),
+(7, 'Universidad', 'ingreso'),
+(8, 'Alimentos', 'gastos'),
+(9, 'Paseo', 'gastos'),
+(10, 'Viajes', 'gastos'),
+(11, 'Educacion', 'ingreso'),
+(12, 'Movilidad', 'gasto'),
+(19, 'Ropa', 'gasto');
 
 -- --------------------------------------------------------
 
@@ -54,6 +62,15 @@ CREATE TABLE `cuentas` (
   `saldo` decimal(10,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `cuentas`
+--
+
+INSERT INTO `cuentas` (`id`, `usuario_id`, `nombre`, `saldo`) VALUES
+(4, 14, 'Interbank', 1200.00),
+(5, 11, 'Scotiabank', 50156.00),
+(6, 13, 'SagaFalabbella', 5026.00);
+
 -- --------------------------------------------------------
 
 --
@@ -65,10 +82,18 @@ CREATE TABLE `transacciones` (
   `categoria_id` int(11) NOT NULL,
   `cuenta_id` int(11) NOT NULL,
   `monto` decimal(10,2) NOT NULL,
-  `tipo` enum('ingreso','gasto') NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
   `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp(),
   `descripcion` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `transacciones`
+--
+
+INSERT INTO `transacciones` (`id`, `categoria_id`, `cuenta_id`, `monto`, `tipo`, `fecha_registro`, `descripcion`) VALUES
+(3, 4, 5, 120.00, 'ingreso', '2025-06-18 04:31:45', 'pago de agua'),
+(5, 7, 5, 530.00, 'gasto', '0000-00-00 00:00:00', 'mensualidad semestral');
 
 -- --------------------------------------------------------
 
@@ -79,10 +104,18 @@ CREATE TABLE `transacciones` (
 CREATE TABLE `transferencias` (
   `id` int(11) NOT NULL,
   `cuenta_id` int(11) DEFAULT NULL,
-  `tipo` enum('envío','recepción') NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
   `cuenta_origen` int(11) DEFAULT NULL,
   `cuenta_destino` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `transferencias`
+--
+
+INSERT INTO `transferencias` (`id`, `cuenta_id`, `tipo`, `cuenta_origen`, `cuenta_destino`) VALUES
+(3, 5, 'envío', 1245622156, 1245589656),
+(4, 6, 'recepcion', 1752147, 1444788);
 
 -- --------------------------------------------------------
 
@@ -105,9 +138,10 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `fecha_registro`) VALUES
 (11, 'graciela', 'ramirez', 'gramirez@gmail.com', '$2y$10$OzRvwtsytHwTXzpN9AnxTu02/uk/PfqJnkEqux5sQdv20VMX/w3aa', '2025-04-17 15:05:42'),
-(12, 'graciela2', 'ramirez', 'gramirez1@gmail.com', '$2y$10$2AfTNYvQVSqLRMeXGL7irOLzIt5JBseHfzX/sFHF.K3XLhyeDmGQe', '2025-04-18 09:53:08'),
-(13, 'graciela2DD', 'ramirez cggbg', 'gramirez451@gmail.com', '$2y$10$8bZs740fZx0JkRj4y.iOsuJNqCfrGmd1yw9dsa7Se4z8wco6UIW6m', '2025-04-22 11:36:53'),
-(14, 'Gregori', 'Bonifas', 'grego1410@gmail.com', '$2y$10$0xPvA4YvpBtmAtXf0ZvT..ArLZ8nISJJcJ4U6W0Rel171UjxfmqL2', '2025-04-27 01:17:27');
+(13, 'Ana Maria', 'Tuesta Ayasta', 'amtuestaa@gmail.com', '$2y$10$Cv9Q3G.M8NCXHFRY977iReJmSS6bZqe9mPWu7n89GinS2sgoKVv9K', '2025-04-22 11:36:53'),
+(14, 'Gregori', 'Bonifas', 'grego1410@gmail.com', '$2y$10$0xPvA4YvpBtmAtXf0ZvT..ArLZ8nISJJcJ4U6W0Rel171UjxfmqL2', '2025-04-27 01:17:27'),
+(15, 'karen', 'flores', 'karen@mvf.com', '$2y$10$vpFCa37/O1JBOA0hfRbug.z1N6ei.R6RjliWNPGAe0p9ZwxnV/6ke', '2025-06-14 13:42:45'),
+(16, 'Bella', 'flores', 'bellaN@mvf.com', '$2y$10$6zxA7acnbaMsq4DWi.doh.e1YFzHwh4cOfxtTj.hf4iA/sbeCInR.', '2025-06-16 11:44:18');
 
 --
 -- Índices para tablas volcadas
@@ -156,31 +190,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `transferencias`
 --
 ALTER TABLE `transferencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
